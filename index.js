740f9cf15343c97f35cdaf3a95533ed8d8bcc18a
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2022-04-26 12:42:30 -0400

    creat variables, commenting on conversion formula

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-"use strict";
+"use strict"; 
+
+// Formula for converting meter to feet. 
+// Meter to feet 
+// 1m = 3.28ft 
+// Mulitply the number of meter by 3.28
+// 
+// Feet to meter
+// 1f= 1m/3.28 = .304m
+// xft * .304m 
+
+let count = document.getElementById("count-el");
+ 
+
+let lengthEl= document.getElementById("length-el");
+
+
+
+// Task to do: 
+// Get the value from the input
+// Check whether the value is actually an integer 
+// Do calculations
