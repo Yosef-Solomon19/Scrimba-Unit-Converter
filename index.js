9f573a10c153f8b0244ef19de2b4db3ddff30220
Yosef-Solomon19 <ysolomon@terpmail.umd.edu>
2022-04-28 16:25:23 -0400

    Added function to receive number from input field. Added logical statement to check if the number is a postive value.

--- index.js
+++ index.js
@@ -9,10 +9,38 @@
 // 1f= 1m/3.28 = .304m
 // xft * .304m 
 
-let count = document.getElementById("count-el");
- 
-
+let count; 
 let lengthEl= document.getElementById("length-el");
+
+const countEl = document.getElementById("count-el");
+
+function getNumber() {
+    count = Number(countEl.value);
+    if (count < 0) {
+        alert("Please type in positive values.")
+        count = 0;
+        console.log(`reset count to ${count}.`);
+        countEl.textContent = 0;
+    } else {
+        console.log(`Postive value: ${count}`);
+        console.log(typeof(count));
+
+    }
+    
+    // console.log(countEl.value);
+    // console.log(typeof(countEl.value));
+
+    // count = Number(countEl.value);
+    console.log(`Check count: ${count}.`);
+    console.log(typeof(count));
+
+    // console.log(typeof(countEl));
+    // console.log(countEl.value)
+    // console.log(typeof(countEl.value));
+    // lengthEl.textContent = Number(countEl.value) + 1 ;    
+    
+}  
+console.log("Hello" + countEl.value);
 
 
 
